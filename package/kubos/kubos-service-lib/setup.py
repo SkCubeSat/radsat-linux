58bfa9a124fab228917b890fab728e92ca958773
RADSATSK2 Machine User <291655798+RADSATMachineUser@users.noreply.github.com>
2026-06-08 20:07:46 -0600

    Update python

---
+++ package/kubos/kubos-service-lib/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+
+setup(
+    name="kubos_service",
+    version="1.22.0",
+    packages=["kubos_service"],
+    license="Apache-2.0",
+    description="Python interface for creating Kubos services",
+)
